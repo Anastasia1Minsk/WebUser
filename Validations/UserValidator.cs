3b62b16538d536c58888bff44fcba3baa74e4889
Anastasia1Minsk <nastia.minsk.by@gmail.com>
2023-10-11 13:13:12 +0300

    add migration + userValidator

---
+++ Validations/UserValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using WebUser.ModelsDto;
+
+namespace WebUser.Validations
+{
+    public class UserValidator : AbstractValidator<UserDto>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("Required field");
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Required field");
+            RuleFor(x => x.Age)
+                .NotEmpty()
+                .WithMessage("Required field")
+                .GreaterThan(-1)
+                .WithMessage("Impossible");
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Required field")
+                .EmailAddress()
+                .WithMessage("Please fill correctly");
+            RuleFor(x => x.Roles)
+                .NotEmpty()
+                .WithMessage("Required");
+        }
+    }
+}
